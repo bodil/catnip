77f7f891b547aa46578d82153a52b640bf74d95b
Bodil Stokke <bodil@bodil.tv>
2012-04-13 14:18:48 +0200

    Run server in the project's context, with the correct dependencies on the classpath.

--- plugin/src/leiningen/edit.clj
+++ plugin/src/leiningen/edit.clj
@@ -1,14 +1,30 @@
 (ns leiningen.edit
-  (:require [mizugorou.server :as server]
-            [clojure.java.browse :as browse])
-  (:import [java.util.concurrent CountDownLatch]))
+  (:require [leiningen.core.eval :as eval])
+  (:import [java.util.concurrent CountDownLatch]
+           [java.net ServerSocket]))
+
+(defn- with-mizugorou-dep [project]
+  (assoc project :dependencies
+         (conj (:dependencies project)
+               '[mizugorou "0.1.0-SNAPSHOT"])))
+
+(defn- start-server-form [port]
+  `(let [url# (str (mizugorou.server/start ~port))]
+     (println "Mizugorou running on" url#)
+     (clojure.java.browse/browse-url url#)))
+
+(defn- server-in-project [project port]
+  (eval/eval-in-project project
+                        (start-server-form port)
+                        `(require 'clojure.java.browse
+                                  'mizugorou.server)))
+
+(defn- free-port []
+  (with-open [socket (ServerSocket. 0)]
+    (.getLocalPort socket)))
 
 (defn edit
   "Launch a Mizugorou server."
   [project & args]
-  (let [[port-str] args
-        port (if port-str (Integer. port-str) 1337)
-        url (str (server/start port))]
-    (println "Mizugorou running on" url)
-    (browse/browse-url url))
+  (server-in-project (with-mizugorou-dep project) (free-port))
   (.await (CountDownLatch. 1)))
