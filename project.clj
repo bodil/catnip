0510a32ae13b04ec4d7310d75954959652158d2d
Yoshito Komatsu <ykomatsu@akaumigame.org>
2012-06-23 23:09:38 +0900

    Removed a dependency on clojure-contrib

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Mozilla Public License"
             :url "http://www.mozilla.org/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.webbitserver/webbit "0.4.6"]
                  [org.clojure/clojurescript "0.0-1011"]
