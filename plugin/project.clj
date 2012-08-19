64cfd91d3115ef73bad22c34992a94d1200a03dc
Bodil Stokke <bodil@bodil.tv>
2012-08-19 22:24:24 +0200

    Bump to next snapshot.

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,8 +1,8 @@
-(defproject lein-catnip "0.2.0"
+(defproject lein-catnip "0.3.0-SNAPSHOT"
   :description "The irresistible Clojure IDE-in-a-plugin"
   :url "http://github.com/bodil/catnip"
   :license {:name "Mozilla Public License"
             :url "http://www.mozilla.org/MPL/2.0/"}
-  :dependencies [[catnip "0.2.0"]
+  :dependencies [[catnip "0.3.0-SNAPSHOT"]
                  [leinjacker "0.2.0"]]
   :eval-in-leiningen true)
