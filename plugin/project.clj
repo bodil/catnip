601d96443b8c3b56127cd85123b4924acc7dbb74
Bodil Stokke <bodil@bodil.tv>
2012-11-17 10:14:16 -0500

    0.5.0 release bump.

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,8 +1,8 @@
-(defproject lein-catnip "0.5.0-SNAPSHOT"
+(defproject lein-catnip "0.5.0"
   :description "The irresistible Clojure IDE-in-a-plugin"
   :url "http://github.com/bodil/catnip"
   :license {:name "Mozilla Public License"
             :url "http://www.mozilla.org/MPL/2.0/"}
-  :dependencies [[catnip "0.5.0-SNAPSHOT"]
+  :dependencies [[catnip "0.5.0"]
                  [leinjacker "0.2.0"]]
   :eval-in-leiningen true)
