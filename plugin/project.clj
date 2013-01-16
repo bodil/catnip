d31f2f11bf2d3b450f039dbbcebe8266c52b773f
Bodil Stokke <bodil@bodil.tv>
2013-01-16 13:10:59 +0100

    Development bump.

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,8 +1,8 @@
-(defproject lein-catnip "0.5.1"
+(defproject lein-catnip "0.6.0-SNAPSHOT"
   :description "The irresistible Clojure IDE-in-a-plugin"
   :url "http://github.com/bodil/catnip"
   :license {:name "Mozilla Public License"
             :url "http://www.mozilla.org/MPL/2.0/"}
-  :dependencies [[catnip "0.5.1"]
+  :dependencies [[catnip "0.6.0-SNAPSHOT"]
                  [leinjacker "0.4.1"]]
   :eval-in-leiningen true)
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject catnip "0.5.1"
+(defproject catnip "0.6.0-SNAPSHOT"
   :plugins [[lein-cljsbuild "0.2.10"]
             [lein-exec "0.2.1"]]
   :description "The irresistible Clojure IDE-in-a-plugin"
