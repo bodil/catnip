a4419f7a7a64712057998328b03067e10cc781cb
Bodil Stokke <bodil@bodil.tv>
2013-01-16 13:13:28 +0100

    HTTPS++

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,6 +1,6 @@
 (defproject lein-catnip "0.6.0-SNAPSHOT"
   :description "The irresistible Clojure IDE-in-a-plugin"
-  :url "http://github.com/bodil/catnip"
+  :url "https://github.com/bodil/catnip"
   :license {:name "Mozilla Public License"
             :url "http://www.mozilla.org/MPL/2.0/"}
   :dependencies [[catnip "0.6.0-SNAPSHOT"]
